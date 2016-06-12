c18dc978ee7f940359c9b4b46ae7ab41a8142fe3
snailzzang <im.your.energy@gmail.com>
2016-06-12 15:05:51 +0900

    주석처리

--- traverse.py
+++ traverse.py
@@ -47,13 +47,14 @@ class PythonTest(unittest.TestCase):
 	        }
 	    }
 
-	    # def find_deepest_child(unix_tree):
+	    # def find_deepest_child(unix_tree):  (가장 깊은 레벨의 child node를 찾는 함수를 구현하시오)
 
-	    # def find_nodes_that_contains_more_than_three_children(unix_tree):
+	    # def find_nodes_that_contains_more_than_three_children(unix_tree): (자식 노드가 3개 이상인 노드 리스트를 구하는 함수)
 
-	    # def count_of_all_distributions_of_linux(unix_tree): 
+	    # def count_of_all_distributions_of_linux(unix_tree): ('리눅스'의 모든 하위 노드는 총 몇개인가)
 
-	    # 아래의 unitest를 통과시킬 수 있도록, 위에 3가지 함수를 짜야하는데, 어렵군요. 재귀함수나 트리 순회 같은 걸 해야 할 것 같은 느낌인데...ㅠ
+	    # 아래의 unitest를 통과시킬 수 있도록, 위에 3가지 함수를 짜야하는데, 어렵군요. 
+	    # 재귀함수나 트리 순회 같은 걸 해야 할 것 같은 느낌인데...ㅠ
 
 	    self.assertEqual('OpenSolaris', find_deepest_child(unix_tree))
 	    self.assertSetEqual({'Unix', 'BSD', 'Linux'}, find_nodes_that_contains_more_than_three_children(unix_tree))
