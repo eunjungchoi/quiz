a28e8666d0a6bdee5a1a603203bcac9facd4ea7a
eunjungchoi <im.your.energy@gmail.com>
2016-06-13 21:47:26 +0900

    주석처리

--- json.py
+++ json.py
@@ -1,40 +1,29 @@
 
 
-import unittest
+def test_json(self):
+	data = '''{
+				"glossary": {
+				    "title": "example glossary",
+					"GlossDiv": {
+				        "title": "S",
+						"GlossList": {
+				            "GlossEntry": {
+				                "ID": "SGML",
+								"SortAs": "SGML",
+								"GlossTerm": "Standard Generalized Markup Language",
+								"Acronym": "SGML",
+								"Abbrev": "ISO 8879:1986",
+								"GlossDef": {
+				                    "para": "A meta-markup language, used to create markup languages such as DocBook.",
+									"GlossSeeAlso": ["GML", "XML"]
+				                },
+								"GlossSee": "markup"
+				            }
+				        }
+				    }
+				}
+			}'''
 
-class PythonTest(unittest.TestCase):
-
-	maxDiff = None
-
-	def test_json(self):
-		data = '''{
-					"glossary": {
-					    "title": "example glossary",
-						"GlossDiv": {
-					        "title": "S",
-							"GlossList": {
-					            "GlossEntry": {
-					                "ID": "SGML",
-									"SortAs": "SGML",
-									"GlossTerm": "Standard Generalized Markup Language",
-									"Acronym": "SGML",
-									"Abbrev": "ISO 8879:1986",
-									"GlossDef": {
-					                    "para": "A meta-markup language, used to create markup languages such as DocBook.",
-										"GlossSeeAlso": ["GML", "XML"]
-					                },
-									"GlossSee": "markup"
-					            }
-					        }
-					    }
-					}
-				}'''
-		self.assertEqual('Standard Generalized Markup Language', pick_GlossTerm(data))
-
-
-
-if __name__ == '__main__':
-    unittest.main()
 
 
 # stackoverflow에서 찾은 답 ----------------------------------------
@@ -48,3 +37,6 @@ def pick_GlossTerm(data):
         return a_dict
     json.loads(data, object_hook=_decode_dict)  
     return results[0]
+
+
+pick_GlossTerm(data)
