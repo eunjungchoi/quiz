5f3e73e3bbdc6bd63c56395feadfdfc9a1f24668
eunjungchoi <im.your.energy@gmail.com>
2016-06-13 21:42:56 +0900

    error correcting

--- permutation.py
+++ permutation.py
@@ -1,23 +1,28 @@
 
-def permutation(char_list):
-    final_list = []
 
+def permutation(char_list):
     if len(char_list) == 0:
         return []
     if len(char_list) == 1:
-        return char_list
+        return [char_list]
+
     
-    each_list =[]
+    final_list = []
     for i, key in enumerate(char_list):
-        #each_list = []
-        each_list.append(key)
-        
+
+        # copy all from char_list except key
         new_char_list = char_list[:]
         new_char_list.pop(i)
+        #new_char_list = [k for k in char_list if k != key]
         
-        each_list.extend(permutation(new_char_list))
-    
-    final_list.append(each_list)
+        all_subperms = permutation(new_char_list) # [[b,c], [c,b]]
+        
+        #
+        for subperm in all_subperms:
+#             each_list = [key]
+#             each_list.extend(subperms[j]) # [a,b,c]   
+            each_list = ([key] + subperm) # a + [b,c]
+            final_list.append(each_list) # [[a,b,c]
     
     return final_list
 
@@ -25,23 +30,23 @@ print(permutation([]))
 
 print(permutation(['a']))
 
-
-print(permutation(['a', 'b']))
+print(permutation([1, 2]))
 
 # sorted(a) == sorted([
 #     ['a', 'b'],
 #     ['b', 'a']
 # ])
 
-print(permutation(['a', 'b', 'c']))
-# sorted(a) == sorted([
-#     ['a', 'b', 'c'],
-#     ['a', 'c', 'b'],
-#     ['b', 'a', 'c'],
-#     ['b', 'c', 'a'],
-#     ['c', 'a', 'b']
-# ])
+print(sorted(permutation(['a', 'b', 'c'])) == sorted([
+    ['a', 'b', 'c'],
+    ['a', 'c', 'b'],
+    ['b', 'a', 'c'],
+    ['b', 'c', 'a'],
+    ['c', 'a', 'b'],
+    ['c', 'b', 'a']
+]))
 
 
-# if __name__ == '__main__':
-#     unittest.main()
+
+t = (1,2,3)
+t2 = (t[0], t[1], t[2], 4)
